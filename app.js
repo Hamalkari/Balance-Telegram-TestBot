116105632c3731d3ba513528fd371531a5fb9c82
Halmakari <kosha.1997@bk.ru>
2020-10-30 23:07:51 +0300

    Add error handler middleware to app

--- app.js
+++ app.js
@@ -1,15 +1,41 @@
 const express = require("express");
-const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const httpStatus = require("http-status");
+const mongoose = require("mongoose");
+
+const ApiError = require("./api/helpers/ApiError");
+const { errorConverter, errorHandler } = require("./api/helpers/middlewares");
 const apiRoutes = require("./api/routes");
+
+const { port, mongo_uri } = require("./config");
+
+require("./telegram_bot");
 
 const app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+
+mongoose
+  .connect(mongo_uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("MongoDb successfully connected");
+  })
+  .catch((err) => {
+    console.log("Can't connect to MongoDb. Reason - " + err.message);
+  });
 
 app.use("/api", apiRoutes);
 
-module.exports = app;
+// Errors hanlder
+app.use((req, res, next) => {
+  next(new ApiError(httpStatus.NOT_FOUND, "Not found"));
+});
+
+app.use(errorConverter);
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`Server listening server on port - ${port}`);
+});
