17a53d87c2dc192da5056d1b9b427800df8ba39f
Halmakari <kosha.1997@bk.ru>
2020-10-30 20:37:48 +0300

    Update User model

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -2,32 +2,40 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
-  telegram_id: {
-    type: Number,
-    unique: true,
-    required: true,
+const UserSchema = new Schema(
+  {
+    id: {
+      type: Number,
+      unique: true,
+      required: true,
+    },
+    is_bot: {
+      type: Boolean,
+      required: true,
+    },
+    first_name: {
+      type: String,
+      required: true,
+    },
+    last_name: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    language_code: {
+      type: String,
+    },
+    balance: {
+      type: Number,
+      default: 0,
+    },
   },
-  is_bot: {
-    type: Boolean,
-    required: true,
-  },
-  first_name: {
-    type: String,
-    required: true,
-  },
-  last_name: {
-    type: String,
-    required: true,
-  },
-  language_code: {
-    type: String,
-  },
-  balance: {
-    type: Number,
-    default: 0,
-  },
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", UserSchema);
 
