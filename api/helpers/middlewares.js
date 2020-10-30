4aafd173a538b96a35beb87b3042af6e7de82f82
Halmakari <kosha.1997@bk.ru>
2020-10-30 23:04:53 +0300

    Add helper files

---
+++ api/helpers/middlewares.js
@@ -0,0 +1,68 @@
+const httpStatus = require("http-status");
+const ApiError = require("./ApiError");
+const Joi = require("joi");
+const { env } = require("../../config");
+
+const pick = (object, keys) => {
+  return keys.reduce((obj, key) => {
+    if (object && Object.prototype.hasOwnProperty.call(object, key)) {
+      obj[key] = object[key];
+    }
+    return obj;
+  }, {});
+};
+
+const errorConverter = (err, req, res, next) => {
+  let error = err;
+  if (!(error instanceof ApiError)) {
+    const statusCode = error.statusCode || httpStatus.INTERNAL_SERVER_ERROR;
+    const message = error.message || httpStatus[statusCode];
+    error = new ApiError(statusCode, message, false, err.stack);
+  }
+  next(error);
+};
+
+const errorHandler = (err, req, res, next) => {
+  let { statusCode, message } = err;
+
+  if (env === "production" && !err.isOperational) {
+    statusCode = httpStatus.INTERNAL_SERVER_ERROR;
+    message = httpStatus[httpStatus.INTERNAL_SERVER_ERROR];
+  }
+
+  res.locals.errorMessage = err.message;
+
+  const response = {
+    code: statusCode,
+    message,
+    ...(env === "development" && { stack: err.stack }),
+  };
+
+  res.status(statusCode).send(response);
+};
+
+const validate = (schema) => (req, res, next) => {
+  const validSchema = pick(schema, ["params", "query", "body"]);
+  const object = pick(req, Object.keys(validSchema));
+  const { value, error } = Joi.compile(validSchema)
+    .prefs({ errors: { label: "key" } })
+    .validate(object);
+
+  if (error) {
+    const errorMessage = error.details
+      .map((details) => details.message)
+      .join(", ");
+
+    return next(new ApiError(httpStatus.BAD_REQUEST, errorMessage));
+  }
+
+  Object.assign(req, value);
+
+  return next();
+};
+
+module.exports = {
+  errorHandler,
+  errorConverter,
+  validate,
+};
